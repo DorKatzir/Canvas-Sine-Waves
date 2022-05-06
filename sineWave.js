6e77baeeae2ac646e730a03e44f3e86fd4679fa4
Dor Katzir <katzirdor@gmail.com>
2022-05-06 19:47:07 +0300

    refined version

--- sineWave.js
+++ sineWave.js
@@ -9,14 +9,14 @@ function initCanvas() {
                     y: cH/2, 
                     length: 0.01, 
                     amplitude: 100,
-                    frequency: 0.04
+                    frequency: 0.03
                 }
     const strokeColor = { 
-                    h: 200,
+                    h: 357,
                     s: 100,
                     l: 50
                 }
-    const fillColor = { 
+    const bgColor = { 
                     r: 0,
                     g: 0,
                     b: 0,
@@ -31,43 +31,38 @@ function initCanvas() {
     waveFolder.open()
 
     const strokeFolder = gui.addFolder('strokeColor')
-    strokeFolder.add(strokeColor, 'h', 0, 255 )
+    strokeFolder.add(strokeColor, 'h', 0, 357 )
     strokeFolder.add(strokeColor, 's', 0, 100 )
     strokeFolder.add(strokeColor, 'l', 0, 100 )
     strokeFolder.open()
     
-    const fillFolder = gui.addFolder('fillColor')
-    fillFolder.add(fillColor, 'r', 0, 255 )
-    fillFolder.add(fillColor, 'g', 0, 255 )
-    fillFolder.add(fillColor, 'b', 0, 255 )
-    fillFolder.add(fillColor, 'a', 0, 1 )
-    fillFolder.open()
+    const bgFolder = gui.addFolder('bgColor')
+    bgFolder.add(bgColor, 'r', 0, 255 )
+    bgFolder.add(bgColor, 'g', 0, 255 )
+    bgFolder.add(bgColor, 'b', 0, 255 )
+    bgFolder.add(bgColor, 'a', 0, 1 )
+    bgFolder.open()
 
     let increment = wave.frequency
+
     function animate() {
         requestAnimationFrame(animate, 5)
         //ctx.save()
         //ctx.fillStyle = '#2b2b2b'
-        ctx.setLineDash([2, 2, 2, 2, 2])
-        //ctx.lineDashOffset = 30
+        ctx.setLineDash([1.68, 3])
+        //ctx.setLineDash([201.68, 101.68])
+        //ctx.lineWidth = 1
+        ctx.lineCap = 'round'
         //ctx.getLineDash()
-        ctx.fillStyle = `rgba(${fillColor.r},${fillColor.g},${fillColor.b},${fillColor.a})`
+        ctx.fillStyle = `rgba(${bgColor.r},${bgColor.g},${bgColor.b},${bgColor.a})`
         ctx.fillRect(0, 0, cW, cH)
         // Draw Here
         ctx.beginPath()
         ctx.moveTo(0, cH / 2)
         for (let i = 0; i < cW; i++) {
-            ctx.lineTo(
-                i,
-                wave.y +
-                    Math.sin(i * wave.length + increment) *
-                        wave.amplitude *
-                        Math.sin(increment * 2)
-            )
+            ctx.lineTo( i, wave.y + Math.sin(i * wave.length + increment) * wave.amplitude * Math.sin(increment * 2) )
         }
-        ctx.strokeStyle = `hsl(${Math.abs(
-            strokeColor.h * Math.sin(increment)
-        )},${strokeColor.s}%,${strokeColor.l}%)`
+        ctx.strokeStyle = `hsl( ${strokeColor.h * Math.abs(Math.sin(increment))} ,${strokeColor.s}%,${strokeColor.l}%)`
         ctx.stroke()
         increment += wave.frequency
         //console.log('animate...')
